67186efdf2261fa05b7f1fc550279dde4f88f0e8
layne <layne@126.com>
2022-03-22 16:12:22 +0800

    Initial commit

---
+++ module01/src/com/module/bean/customer.java
@@ -0,0 +1,7 @@
+package com.module.bean;
+
+public class customer {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+    }
+}
---
+++ src/com/demo2/java/helloworld.java
@@ -0,0 +1,12 @@
+package com.demo2.java;
+
+import java.util.ArrayList;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        ArrayList list =new ArrayList();
+
+    }
+
+}
